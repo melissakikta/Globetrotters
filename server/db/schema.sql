62f62b9ae13ce015bd57851a42d8d6e5602ef27e
ivandamian1 <adroverivan7@gmail.com>
2025-01-28 23:15:26 -0500

    fixedserver

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -7,19 +7,19 @@ CREATE DATABASE globetrotters_db;
 
 
 
-DROP DATABASE IF EXISTS bucketlist_db;
-CREATE DATABASE bucketlist_db;
--- Create tables
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
-);  
+-- DROP DATABASE IF EXISTS bucketlist_db;
+-- CREATE DATABASE bucketlist_db;
+-- -- Create tables
+-- CREATE TABLE users (
+--     id SERIAL PRIMARY KEY,
+--     email VARCHAR(255) UNIQUE NOT NULL,
+--     password VARCHAR(255) NOT NULL
+-- );  
 
-CREATE TABLE bucket_list (
-    id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id) ON DELETE CASCADE,
-    country VARCHAR(255) NOT NULL,
-    item VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+-- CREATE TABLE bucket_list (
+--     id SERIAL PRIMARY KEY,
+--     user_id INT REFERENCES users(id) ON DELETE CASCADE,
+--     country VARCHAR(255) NOT NULL,
+--     item VARCHAR(255) NOT NULL,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
